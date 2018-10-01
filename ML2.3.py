a436a329febca02c1c548a49ba60126bb69d5bee
yongmin.ko <euclidmin@gmail.com>
2018-10-01 19:46:02 +0900

    KNeighborsClassifier 분석  코드

--- ML2.3.py
+++ ML2.3.py
@@ -37,14 +37,68 @@ import numpy as np
 
 
 
-from sklearn.datasets import load_boston
-boston = load_boston()
-print("데이터의 형태:{}".format(boston.data.shape))
-
-X, y = mglearn.datasets.load_extended_boston()
-print("X.shape:{}".format(X.shape))
+# from sklearn.datasets import load_boston
+# boston = load_boston()
+# print("데이터의 형태:{}".format(boston.data.shape))
+#
+# X, y = mglearn.datasets.load_extended_boston()
+# print("X.shape:{}".format(X.shape))
 
 
 
 #### 2.3.2 K-최근접 이웃
-mglearn.plots.plot_knn_classification(n_neighbors=1)
+# mglearn.plots.plot_knn_classification(n_neighbors=1)
+# mglearn.plots.plot_knn_classification(n_neighbors=3)
+
+
+# from sklearn.model_selection import train_test_split
+# X, y =  mglearn.datasets.make_forge()
+# X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+#
+# from sklearn.neighbors import KNeighborsClassifier
+# clf = KNeighborsClassifier(n_neighbors=3)
+# clf.fit(X_train, y_train)
+# print("테스트 세트 예측{}".format(clf.predict(X_test)))
+# print("테스트 세트 정확도: {:.2f}".format(clf.score(X_test, y_test)))
+#
+#
+# fig, axes = plt.subplots(1, 3, figsize=(10, 3))
+#
+# for n_neighbors, ax in zip([1, 3, 9], axes) :
+#     clf = KNeighborsClassifier(n_neighbors=n_neighbors).fit(X, y)
+#     mglearn.plots.plot_2d_separator(clf, X, fill=True, eps=0.5, ax=ax, alpha=.4 )
+#     mglearn.discrete_scatter(X[:, 0], X[:, 1], y, ax=ax)
+#     ax.set_title("{} 이웃".format(n_neighbors))
+#     ax.set_xlabel("특성 0")
+#     ax.set_ylabel("특성 1")
+# axes[0].legend(loc=3)
+# plt.show()
+
+
+
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+cancer = load_breast_cancer()
+X_train, X_test, y_train, y_test = train_test_split(
+    cancer.data, cancer.target, stratify=cancer.target, random_state=66)
+
+training_accuracy = []
+test_accuracy = []
+
+neighbors_settings = range(1, 11)
+for n_neightbors in neighbors_settings:
+    clf = KNeighborsClassifier(n_neighbors=n_neightbors)
+    clf.fit(X_train, y_train)
+    training_accuracy.append(clf.score(X_train, y_train))
+    test_accuracy.append(clf.score(X_test, y_test))
+
+plt.plot(neighbors_settings, training_accuracy, label="훈력 정확도")
+plt.plot(neighbors_settings, test_accuracy, label="테스트 정확도")
+plt.ylabel("정확도")
+plt.xlabel("n_neighbors")
+plt.legend()
+plt.show()
+
+
