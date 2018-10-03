1a346c0acbe71f0af5b3d64d015a8b72b6d0093d
yongmin.ko <euclidmin@gmail.com>
2018-10-03 09:56:36 +0900

    KNeighborsRegressor 분석2

--- ML2.3.py
+++ ML2.3.py
@@ -110,10 +110,31 @@ from sklearn.model_selection import train_test_split
 X, y = mglearn.datasets.make_wave(n_samples=40)
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 
-reg = KNeighborsRegressor(n_neighbors=3)
-reg.fit(X_train, y_train)
-print("테스트 세트 예측:\n{}".format(reg.predict(X_test)))
-print("테스트 세트 R^2:{:.2f}".format(reg.score(X_test, y_test)))
+# reg = KNeighborsRegressor(n_neighbors=3)
+# reg.fit(X_train, y_train)
+# print("테스트 세트 예측:\n{}".format(reg.predict(X_test)))
+# print("테스트 세트 R^2:{:.2f}".format(reg.score(X_test, y_test)))
+
+fig, axes = plt.subplots(1, 3, figsize=(15, 4))
+line = np.linspace(-3, 3, 1000).reshape(-1, 1)
+for n_neighbors, ax in zip([1, 3, 9], axes) :
+    reg = KNeighborsRegressor(n_neighbors=n_neighbors)
+    reg.fit(X_train, y_train)
+    ax.plot(line, reg.predict(line))
+    ax.plot(X_train, y_train, '^', c=mglearn.cm2(0), markersize=8)
+    ax.plot(X_test, y_test, 'v', c=mglearn.cm2(1), markersize=8)
+
+    ax.set_title(
+        "{} 이웃의 훈련 스코어 : {:.2f} 테스크 스코어:{:.2f}".format(
+            n_neighbors, reg.score(X_train, y_train),
+            reg.score(X_test, y_test)
+        )
+    )
+    ax.set_xlabel("특성")
+    ax.set_ylabel("타깃")
+axes[0].legend(["모델 예측", "훈련 데이터/타깃", "데스트 데이터/타깃"], loc="best")
+plt.show()
+
 
 
 
