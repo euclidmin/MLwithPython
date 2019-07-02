710e0739152d8dc34aad359679290769e330e82e
euclidmin <euclidmin@gmail.com>
2019-07-02 22:28:18 +0900

    pca 사용해서 법 연습

--- ML3.3.py
+++ ML3.3.py
@@ -34,13 +34,52 @@ from sklearn.svm import SVC
 # print(svm.score(X_test_scaled, y_test))
 # # ==> 차이가 엄청나다.
 
-svm = SVC(100)
-svm.fit(X_train, y_train)
-print(svm.score(X_test, y_test))
+# svm = SVC(100)
+# svm.fit(X_train, y_train)
+# print(svm.score(X_test, y_test))
+#
+# for scalar in [MinMaxScaler(), StandardScaler(), RobustScaler()]:
+#     scalar.fit(X_train)
+#     X_train_scaled = scalar.transform(X_train)
+#     X_test_scaled = scalar.transform(X_test)
+#     svm.fit(X_train_scaled, y_train)
+#     print(svm.score(X_test_scaled, y_test))
 
-for scalar in [MinMaxScaler(), StandardScaler(), RobustScaler()]:
-    scalar.fit(X_train)
-    X_train_scaled = scalar.transform(X_train)
-    X_test_scaled = scalar.transform(X_test)
-    svm.fit(X_train_scaled, y_train)
-    print(svm.score(X_test_scaled, y_test))
+
+from sklearn.datasets import load_breast_cancer
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+
+# cancer =  load_breast_cancer()
+#
+# scaler = StandardScaler()
+# scaler.fit(cancer.data)
+# X_scaled = scaler.transform(cancer.data)
+#
+# pca = PCA(n_components=2)
+# pca.fit(X_scaled)
+#
+# X_pca = pca.transform(X_scaled)
+#
+# print(X_scaled.shape)
+# print(X_pca.shape)
+
+
+# LFW 데이터 셋에 있는 이미지 샘플
+from sklearn.datasets import fetch_lfw_people
+import matplotlib.pyplot as plt
+
+people = fetch_lfw_people(min_faces_per_person=20, resize=0.7)
+image_shape = people.images[0].shape
+
+fig, axes = plt.subplots(2, 5, figsize=(15, 8), subplot_kw={'xticks': (), 'yticks': ()})
+for target, image, ax in zip(people.target, people.images, axes.ravel()):
+    ax.imshow(image)
+    ax.set_title(people.target_names[target])
+plt.show()
+
+import numpy as np
+
+counts = np.bincount(people.target)
+for i, (count, name) in enumerate(zip(counts, people.target_names)):
+    print("{0:25} {1:3}".format(name, count))
